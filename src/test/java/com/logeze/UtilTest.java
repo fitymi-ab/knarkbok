b64522548e89e1929a1711b384f5aa28e153c944
Thomas Sundberg <thomas@thinkcode.se>
2026-01-02 16:50:27 +0100

    Added testing tools

---
+++ src/test/java/com/logeze/UtilTest.java
@@ -0,0 +1,13 @@
+package com.logeze;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilTest {
+
+    @Test
+    void should_verify_test_setup() {
+        assertThat(true).isTrue();
+    }
+}

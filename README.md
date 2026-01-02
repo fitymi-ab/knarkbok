c28db0873bdb087ca1c417880c28ab3797774ddc
Thomas Sundberg <thomas@thinkcode.se>
2026-01-02 16:29:06 +0100

    Added a proper website served by Javalin

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # knarkbok
+
 Knarkbok / Drug journal web site
+
+## Build
+
+```
+./mvnw clean package
+```
+
+## Run
+
+```
+java -jar ./target/knarkbok-site-1.0.0.jar
+```
+
+## Java 25
+
+```
+sdk use java 25-zulu
+```
+
+## Java 21
+
+```
+sdk use java 21-zulu
+```
--- src/main/java/com/logeze/Main.java
+++ src/main/java/com/logeze/Main.java
@@ -1,7 +1,12 @@
 package com.logeze;
 
+import io.javalin.Javalin;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    static void main() {
+        int port = 7070;
+
+        Javalin.create(javalinConfig -> javalinConfig.staticFiles.add("/site"))
+                .start(port);
     }
 }
